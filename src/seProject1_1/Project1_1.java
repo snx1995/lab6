fe7dd511afceed0bdf416d3e820561cac57bb329
snx1995 <snx1788@gmail.com>
2017-09-15 19:35:18 +0800

    完成了桥接词的代码和测试，随机游走完成550%

--- src/seProject1_1/Project1_1.java
+++ src/seProject1_1/Project1_1.java
@@ -7,45 +7,54 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Vector;
 
-
-
-
 public class Project1_1 {
-	
+
 	public static void main(String[] args) {
-		Graph g1=createDirectedGraph("data.txt");
-		g1.cons_debug()	;
-		System.out.println(queryBridgeWords(g1,"new","and"));
-		showDirectedGraph(g1);
+		String word1 = "to";
+		String word2 = "to";
+		Graph g1 = createDirectedGraph("data.txt");
+		g1.cons_debug();
+		System.out.println(queryBridgeWords(g1, word1, word2));
+		for (String word : g1.getBridgeWords(word1, word2)) {
+			System.out.println(word);
+		}
+		System.out.println(generateNewText(g1, "new and"));
+		// showDirectedGraph(g1);
+		for(int i=0;i<g1.get_v_number();i++) {
+			System.out.println(g1.getEdgeNum(i));
+		}
+		System.out.println(randomWalk(g1));
 	}
-	
-//合并字符数组a,b
+
+	// 合并字符数组a,b
 	static String[] concat(String[] a, String[] b) {
 		String[] c = new String[a.length + b.length];
 		System.arraycopy(a, 0, c, 0, a.length);
 		System.arraycopy(b, 0, c, a.length, b.length);
 		return c;
 	}
-	
-//创建邻接表
+
+	// 创建邻接表
 	public static Graph createDirectedGraph(String filename) {
-		//wordtmp保存并分割一行，words保存全部单词
+		// wordtmp保存并分割一行，words保存全部单词
 		String[] wordtmp = null, words = {};
 		try {
 			FileReader fr = new FileReader(filename);
 			BufferedReader br = new BufferedReader(fr);
 			String line = null;
 			while ((line = br.readLine()) != null) {
-				//line=line.toLowerCase();
-				//分割方式：以一个或多个全部非小写字母字符为分割点
+				// line=line.toLowerCase();
+				// 分割方式：以一个或多个全部非小写字母字符为分割点
 				wordtmp = line.split("[^a-zA-Z]+");
-				for(int i=0;i<wordtmp.length;i++) {
-					wordtmp[i]=wordtmp[i].toLowerCase();	
+				for (int i = 0; i < wordtmp.length; i++) {
+					wordtmp[i] = wordtmp[i].toLowerCase();
 				}
 				words = concat(words, wordtmp);
-			}br.close();
+			}
+			br.close();
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		} catch (IOException e2) {
@@ -54,49 +63,34 @@ public class Project1_1 {
 		Graph graph = new Graph(words);
 		return graph;
 	}
-	//输出G中word1和word2的桥接词
-	static String queryBridgeWords(Graph G, String word1, String word2) {
-		int word1_num=G.get_word_place(word1);
-		int word2_num=G.get_word_place(word2);
-		if(word1_num<0 || word2_num<0) 
-			return "No "+word1+" or "+word2+" in the graph!";
-		G_List lists[] = G.get_lists();
-		G_List tmp1=lists[word1_num];
-		//System.out.println(word2_num);
-		int bridge[]= {};
-		while(tmp1!=null) {
-			G_List tmp2=lists[tmp1.word_place];
-			while(tmp2!=null) {
-				if(tmp2.word_place==word2_num) {
-					bridge=Arrays.copyOf(bridge, bridge.length+1);
-					bridge[bridge.length-1]=tmp1.word_place;
-					break;
-				}
-				tmp2=tmp2.next;
-			}
-			tmp1=tmp1.next;
 
+	// 输出G中word1和word2的桥接词
+	static String queryBridgeWords(Graph G, String word1, String word2) {
+		String[] bridge = G.getBridgeWords(word1, word2);
+		if (bridge == null)
+			return "No " + word1 + " or " + word2 + " in the graph!";
+		if (bridge.length == 0)
+			return "No bridge words from " + word1 + " to " + word2 + "!";
+		if (bridge.length == 1)
+			return "The bridge words from " + word1 + " to " + word2 + " is: " + bridge[0];
+		String result = "The bridge words from " + word1 + " to " + word2 + " are: ";
+		for (int i = 0; i < bridge.length - 1; i++) {
+			result = result + bridge[i] + ", ";
 		}
-		if(bridge.length==0)
-			return "No bridge words from "+word1+" to "+word2+"!";
-		if(bridge.length==1)
-			return "The bridge words from "+word1+" to "+word2+" is: "+G.get_vector()[bridge[0]];
-		String result="The bridge words from "+word1+" to "+word2+" are: ";
-		for(int i=0;i<bridge.length-1;i++) {
-			result=result+G.get_word(bridge[i])+", ";
-		}
-		result = result+"and "+G.get_word(bridge[bridge.length-1])+".";
+		result = result + "and " + bridge[bridge.length - 1] + ".";
 		return result;
 	}
+
 	static void showDirectedGraph(Graph G) {
-		GraphViz gViz = new GraphViz("E:\\Projects\\java\\SEProject_1", "C:\\Program Files (x86)\\Graphviz2.38\\bin\\dot.exe");
+		GraphViz gViz = new GraphViz("E:\\Projects\\java\\SEProject_1",
+				"C:\\Program Files (x86)\\Graphviz2.38\\bin\\dot.exe");
 		gViz.start_graph();
-		for(G_List list:G.get_lists()) {
-			if(list.next!=null) {
-				G_List tmp=list.next;
-				while(tmp!=null) {
-					gViz.addln(G.get_vector()[list.word_place]+"->"+G.get_vector()[tmp.word_place]+";");
-					tmp=tmp.next;
+		for (G_List list : G.get_lists()) {
+			if (list.next != null) {
+				G_List tmp = list.next;
+				while (tmp != null) {
+					gViz.addln(G.get_vector()[list.word_place] + "->" + G.get_vector()[tmp.word_place] + ";");
+					tmp = tmp.next;
 				}
 			}
 		}
@@ -107,8 +101,59 @@ public class Project1_1 {
 			e.printStackTrace();
 		}
 	}
+
+	static String generateNewText(Graph G, String inputText) {
+		Random random = new Random();
+		String[] letters = inputText.split("[^a-zA-Z]+");
+		String[] bridgeWords = null;
+		for (int i = 0; i < letters.length; i++)
+			letters[i] = letters[i].toLowerCase();
+		String newText = letters[0];
+		for (int i = 1; i < letters.length; i++) {
+			bridgeWords = G.getBridgeWords(letters[i - 1], letters[i]);
+			if (bridgeWords == null || bridgeWords.length == 0) {
+				newText = newText + " " + letters[i];
+			} else if (bridgeWords.length == 1) {
+				newText = newText + " " + bridgeWords[0] + " " + letters[i];
+			} else {
+				newText = newText + " " + bridgeWords[random.nextInt(bridgeWords.length)] + " " + letters[i];
+			}
+		}
+		return newText;
+	}
+
+	static String randomWalk(Graph G) {
+		int n = G.get_v_number();
+		boolean[][] edgeVisited = new boolean[n][n];
+		
+		Random random = new Random();
+		int firstWord = random.nextInt(n);
+		G_List lists[] = G.get_lists();
+		String walkPath = G.get_word(firstWord) + " ";
+		G_List tmp=lists[firstWord];
+		int edge = random.nextInt(G.getEdgeNum(firstWord));
+		for(int i=0;i<edge;i++) {
+			tmp=tmp.next;
+		}
+		int nextWord = tmp.word_place;
+		while(!edgeVisited[firstWord][nextWord]) {
+			edgeVisited[firstWord][nextWord]=true;
+			System.out.println(firstWord + "-" + nextWord);
+			tmp=lists[firstWord];
+			walkPath = walkPath + G.get_word(nextWord)+" ";
+			firstWord=nextWord;
+			edge = random.nextInt(G.getEdgeNum(firstWord));
+			for(int i=0;i<edge;i++) {
+				tmp=tmp.next;
+			}
+			nextWord = tmp.word_place;
+		}
+		//to explore strange new worlds seek out life and civilizations this 
+		return walkPath;
+	}
 }
-//图节点
+
+// 图节点
 class G_List {
 	public int word_place;
 	public G_List next;
@@ -123,13 +168,15 @@ class G_List {
 	public G_List() {
 	}
 }
-//图
+
+// 图
 class Graph {
 	private String vector[] = {};
 	private int v_number, n_number;
 	private G_List lists[] = {};
-	//存储word-》word_place
+	// 存储word-》word_place
 	private Map<String, Integer> letters = new HashMap<String, Integer>();
+
 	public Graph(String words[]) {
 		int pre = -1;
 		n_number = words.length;
@@ -178,39 +225,81 @@ class Graph {
 	}
 
 	public String cons_debug() {
-		for(String v:vector) {
+		for (String v : vector) {
 			System.out.print(v + " ");
 		}
 		System.out.println();
-		for(G_List list:lists) {
-			G_List temp=list;
-			while(temp!=null) {
-				System.out.print(temp.word_place+" "+temp.cost+"/");
-				temp=temp.next;
+		for (G_List list : lists) {
+			G_List temp = list;
+			while (temp != null) {
+				System.out.print(temp.word_place + " " + temp.cost + "/");
+				temp = temp.next;
 			}
 			System.out.println();
 		}
 		return "";
 	}
+
+	public String[] getBridgeWords(String word1, String word2) {
+		String[] bridgeWords = {};
+		int word1_place = letters.containsKey(word1) ? letters.get(word1) : -1;
+		int word2_place = letters.containsKey(word2) ? letters.get(word2) : -1;
+		if (word1_place < 0 || word2_place < 0) {
+			return null;
+		}
+		G_List tmp1 = lists[word1_place].next;
+		while (tmp1 != null) {
+			G_List tmp2 = lists[tmp1.word_place].next;
+			while (tmp2 != null) {
+				if (tmp2.word_place == word2_place) {
+					bridgeWords = Arrays.copyOf(bridgeWords, bridgeWords.length + 1);
+					bridgeWords[bridgeWords.length - 1] = vector[tmp1.word_place];
+					break;
+				}
+				tmp2 = tmp2.next;
+			}
+			tmp1 = tmp1.next;
+		}
+		return bridgeWords;
+	}
+
 	public String[] get_vector() {
 		return vector;
 	}
+
 	public int get_v_number() {
 		return v_number;
 	}
+
 	public int get_n_number() {
 		return n_number;
 	}
+
 	public G_List[] get_lists() {
 		return lists;
 	}
+
 	public int get_word_place(String word) {
-		if(letters.containsKey(word))
+		if (letters.containsKey(word))
 			return letters.get(word);
 		else
 			return -1;
 	}
+
 	public String get_word(int place) {
 		return vector[place];
 	}
+	
+	public int getEdgeNum(int wordPlace) {
+		int count = 0;
+		G_List tmp=lists[wordPlace];
+		while(tmp!=null) {
+			if(tmp.next!=null) {
+				count++;
+			}
+			tmp=tmp.next;
+		}
+		return count;
+	}
+
 }
